60c98539be0cfab08274f11af74e5ca0cbc58b01
fengjc <jcfeng2013@gmail.com>
2019-06-03 17:30:10 +0800

    change test.py

--- test.py
+++ test.py
@@ -4,11 +4,11 @@
 Project: dockerTest
 Author: jc feng (jcfeng2013@gmail.com)
 File Created: 2019-06-03 11:45:21
-Last Modified: 2019-06-03 13:09:07
+Last Modified: 2019-06-03 17:28:44
 '''
 
 import requests
 
 
 def test_hello():
-    assert 'hello docker' == requests.get('http://127.0.0.1:5000/').text
+    assert 'hello world' == requests.get('http://127.0.0.1:5000/').text
