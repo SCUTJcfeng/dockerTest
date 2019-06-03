9e3cbcb93eb2877a3e9bee7313976b0ec9c11e26
fengjc <jcfeng2013@gmail.com>
2019-06-03 21:32:16 +0800

    add redis connect test

--- app/app/views.py
+++ app/app/views.py
@@ -4,13 +4,19 @@
 Project: app
 Author: jc feng (jcfeng2013@gmail.com)
 File Created: 2019-06-03 13:53:35
-Last Modified: 2019-06-03 13:54:24
+Last Modified: 2019-06-03 21:31:38
 '''
 
-
+import redis
 from app import app
 
 
 @app.route('/')
 def hello():
     return 'hello world'
+
+
+@app.route('/redis')
+def redis_hello():
+    r = redis.Redis(host="redis", port=6379)
+    return r.info()['rdb_last_save_time']
--- test.py
+++ test.py
@@ -4,7 +4,7 @@
 Project: dockerTest
 Author: jc feng (jcfeng2013@gmail.com)
 File Created: 2019-06-03 11:45:21
-Last Modified: 2019-06-03 17:28:44
+Last Modified: 2019-06-03 21:29:15
 '''
 
 import requests
@@ -12,3 +12,7 @@ import requests
 
 def test_hello():
     assert 'hello world' == requests.get('http://127.0.0.1:5000/').text
+
+
+def test_redis():
+    print(requests.get('http://127.0.0.1:5000/redis').text)
