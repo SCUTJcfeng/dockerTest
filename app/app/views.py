e569bbe54a226463a0a1a8ee19b24ad340ac1072
fengjc <jcfeng2013@gmail.com>
2019-06-03 21:40:49 +0800

    change test

--- app/app/views.py
+++ app/app/views.py
@@ -4,7 +4,7 @@
 Project: app
 Author: jc feng (jcfeng2013@gmail.com)
 File Created: 2019-06-03 13:53:35
-Last Modified: 2019-06-03 21:31:38
+Last Modified: 2019-06-03 21:40:30
 '''
 
 import redis
@@ -19,4 +19,4 @@ def hello():
 @app.route('/redis')
 def redis_hello():
     r = redis.Redis(host="redis", port=6379)
-    return r.info()['rdb_last_save_time']
+    return str(r.info()['rdb_last_save_time'])
--- test.py
+++ test.py
@@ -4,7 +4,7 @@
 Project: dockerTest
 Author: jc feng (jcfeng2013@gmail.com)
 File Created: 2019-06-03 11:45:21
-Last Modified: 2019-06-03 21:29:15
+Last Modified: 2019-06-03 21:40:17
 '''
 
 import requests
@@ -15,4 +15,4 @@ def test_hello():
 
 
 def test_redis():
-    print(requests.get('http://127.0.0.1:5000/redis').text)
+    assert 200 == requests.get('http://127.0.0.1:5000/redis').status_code
